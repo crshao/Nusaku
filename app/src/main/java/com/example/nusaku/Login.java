19daf21e9c9a2c6f19b049410fb51767b9bafa15
heizzz <gautamabryan@gmail.com>
2020-08-08 22:18:56 +0800

    ui login register

--- app/src/main/java/com/example/nusaku/Login.java
+++ app/src/main/java/com/example/nusaku/Login.java
@@ -1,15 +1,35 @@
 package com.example.nusaku;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Login extends AppCompatActivity {
+    @BindView(R.id.masuk)
+    Button b1;
+    @BindView(R.id.back)
+    Button b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @OnClick(R.id.masuk)
+    void masuk(){
+        startActivity(new Intent(this , MainActivity.class));
+    }
+
+    @OnClick(R.id.back)
+    void back(){
+        startActivity(new Intent(this , Welcome.class));
     }
 }
 
--- app/src/main/java/com/example/nusaku/Register.java
+++ app/src/main/java/com/example/nusaku/Register.java
@@ -1,14 +1,34 @@
-    package com.example.nusaku;
+package com.example.nusaku;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Register extends AppCompatActivity {
+    @BindView(R.id.daftar)
+    Button b1;
+    @BindView(R.id.back)
+    Button b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    @OnClick(R.id.daftar)
+    void daftar(){
+        startActivity(new Intent(this , Welcome.class));
+    }
+
+    @OnClick(R.id.back)
+    void back(){
+        startActivity(new Intent(this , Welcome.class));
     }
 }
