c4cabb53edd8d0af5921fdc0dc6d48ab74f855c8
hao <calvindon08@gmail.com>
2020-08-12 16:11:57 +0700

    Update some gradle version

--- app/src/main/java/com/example/nusaku/ui/beranda/BerandaFragment.java
+++ app/src/main/java/com/example/nusaku/ui/beranda/BerandaFragment.java
@@ -29,6 +29,7 @@ public class BerandaFragment extends Fragment {
                              Bundle savedInstanceState) {
         berandaViewModel = ViewModelProviders.of(this).get(BerandaViewModel.class);
         View root = inflater.inflate(R.layout.fragment_beranda, container, false);
+
         final TextView textView = root.findViewById(R.id.text_beranda);
 
         berandaViewModel.getText().observe(this, new Observer<String>() {
