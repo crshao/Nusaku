cc5b85dc5cedfd889ac9a07d3bdc4aaafa1684e6
hao <calvindon08@gmail.com>
2020-08-17 23:50:24 +0700

    TokenManager + update ApiInterface

--- app/src/main/java/com/example/nusaku/api/ApiInterface.java
+++ app/src/main/java/com/example/nusaku/api/ApiInterface.java
@@ -1,11 +1,14 @@
 package com.example.nusaku.api;
 
+import com.example.nusaku.models.AccessToken;
+import com.example.nusaku.models.DestinationTypeData;
+
 import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiInterface {
-//    @GET("getDestinationType")
-//    Call<ArrayList<>>
+    @GET("getDestinationType")
+    Call<ArrayList<DestinationTypeData>> getDestinationType();
 }
