29fa71c7e98c38ebf13af014837c8ce90b8ed8bc
heizzz <gautamabryan@gmail.com>
2020-08-17 11:16:59 +0800

    minor fix

--- app/src/main/java/com/example/nusaku/Welcome.java
+++ app/src/main/java/com/example/nusaku/Welcome.java
@@ -6,6 +6,9 @@ import androidx.viewpager.widget.ViewPager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+
+import com.example.nusaku.ui.bookmark.BookmarkFragment;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -16,6 +19,8 @@ public class Welcome extends AppCompatActivity {
     Button b1;
     @BindView(R.id.daftar)
     Button b2;
+    @BindView(R.id.cit)
+    Button b3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,11 @@ public class Welcome extends AppCompatActivity {
     @OnClick(R.id.daftar)
     void daftar(){
         startActivity(new Intent(this , Register.class));
+    }
+
+    @OnClick(R.id.cit)
+    void cit(){
+        startActivity(new Intent(null , MainActivity.class));
     }
 
 }
